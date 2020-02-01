21a89db9a316653885d373d9b7b0acc6a10c49bd
eswaribala <Parameswaribala@gmail.com>
2020-02-01 16:59:52 +0530

    Add files via upload

---
+++ retphoto.js
@@ -0,0 +1,34 @@
+/**
+ * Created by BALASUBRAMANIAM on 08-01-2015.
+ */
+
+console.log("loading...");
+for(i=0;i<window.localStorage.length;i++)
+{
+    if (window.localStorage.getItem("photo"+i) != undefined) {
+
+
+        retrieveData(window.localStorage.getItem("photo"+i));
+
+
+    }
+}
+function retrieveData(filename)
+{
+
+    console.log("image available");
+    var img = new Image();
+
+    img.onload = function (evt) {
+        var photoref = document.getElementById("photosection");
+        photoref.appendChild(img);
+        node = document.createElement("hr");
+        photoref.appendChild(node);
+    }
+
+    img.src = filename;
+    img.width=50;
+    img.height=50;
+
+
+}
